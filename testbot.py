9e66016f141e1caa04e8f6d193718415c1990f2b
menchit <thomas.menchi@edu.esiee.fr>
2020-11-20 23:21:11 +0100

    opus'

--- testbot.py
+++ testbot.py
@@ -15,15 +15,6 @@ bot = commands.Bot(command_prefix='|')
 trans = {'p':'pilote', 'a':'astrophysicien','i':'ingenieur','x':'xenobiologiste'}
 
 
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
-
-
 async def join(ctx):
     channel = ctx.author.voice.channel
     return await channel.connect()
