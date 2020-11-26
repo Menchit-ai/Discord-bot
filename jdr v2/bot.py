b8b91b1994b6e4abf2b2d8134fd4c54fb51df507
menchit <thomas.menchi@edu.esiee.fr>
2020-11-26 23:51:23 +0100

    notion de système de jeu, reste à ajouter la suppression et l'ajout des dossiers correspondant pour stocker les informations joueurs

--- jdr v2/bot.py
+++ jdr v2/bot.py
@@ -1,5 +1,8 @@
 # bot.py
 import os
+import shutil
+import pathlib
+
 import random
 import json
 from spellchecker import SpellChecker
@@ -12,7 +15,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='/')
-trans = {'p':'pilote', 'a':'astrophysicien','i':'ingenieur','x':'xenobiologiste','l':'linguiste'}
+
+######  CHEMIN DE BASE  #########
+path_data     = "./data/"
+path_sys_json = "./data/system.json"
+path_sys_txt  = "./data/system.txt"
+path_sound    = "./data/data_sound/"
+#################################
 
 
 async def play(ctx,path):
@@ -35,26 +44,105 @@ async def play(ctx,path):
     except:
         pass
 
+def spellcheck(words):
+    spell = SpellChecker(language=None)
+    spell.word_frequency.load_words(words)
+    return spell
+
+def get_sys(ctx):
+    pass
 
 # toutes les commandes disponibles avec le bot
+@bot.command(name='emb')
+async def emb(ctx):
+    embed=discord.Embed(title="Tile", description="Desc", color=0x00ff00)
+    embed.add_field(name="Fiel1", value="hi", inline=False)
+    embed.add_field(name="Field2", value="hi2", inline=True)
+    await ctx.send(embed=embed)
 
+@bot.command(name='system', aliases=['sys'], help='Permet de définir le système de jeu utilisé dans tout le serveur Discord. option : c (nouveau système)')
+async def change_system(ctx, newstate:str, option:str=None):
 
-@bot.command(name='show', aliases=['s'], help='Debugger.')
+    with open(path_sys_txt) as f:    sys = [i.strip() for i in f.readlines()]
+    spell = spellcheck(sys)
+    try : newstate = spell.correction(newstate)
+    except : pass
+
+    if option is "c":
+        if newstate in sys:
+            await ctx.send(newstate + " est déjà présent dans les systèmes utilisables !")
+            return
+        with open(path_sys_txt,'a') as filesys:
+            w = newstate+'\n'
+            filesys.write(w)
+            await ctx.send(newstate + " a bien été ajouté aux systèmes jouables !")
+
+    if option is "d":
+        with open(path_sys_txt,'w') as filesys:
+            try:
+                sys.remove(newstate)
+            except:
+                await ctx.send(newstate + " ne fait pas partie de la liste.")
+                return
+            filesys.write("\n".join(sys))
+            filesys.write('\n')
+        
+        data = {}
+        with open(path_sys_json,'r') as json_file:
+            try: data = json.load(json_file)
+            except:pass
+        todel = []
+        for key,value in data.items():
+            if value == newstate:
+                todel.append(key)
+        for key in todel:
+            data.pop(key)
+        with open(path_sys_json,'w') as json_file:
+            json.dump(data,json_file)
+        await ctx.send(newstate + " a bien été supprimé de la liste.")
+        return
+    if not (newstate in sys) : await ctx.send("Ce système n'est pas disponible pour le moment."); return
+    data = {}
+    with open(path_sys_json,'r') as json_file:
+        try : data = json.load(json_file)
+        except : data = {}
+    with open(path_sys_json,'w') as json_file:
+        guild = ctx.guild.name
+        data[guild] = newstate
+        json.dump(data,json_file)
+        await ctx.send("le serveur " + guild + " utilise maintenant le système " + newstate + ".")
+        
+    return
+
+@bot.command(name='info', aliases=['i'], help='Informations générales.')
 async def show(ctx):
-    print(ctx.me)
-    print(ctx.me.voice.channel)
+    await ctx.send(ctx.author)
+    await ctx.send(ctx.guild)
+    with open(path_sys_json,'r') as json_file:
+        data = json.load(json_file)
+        sys = 'None'
+        try : sys = data[ctx.guild.name]
+        except : pass
+        await ctx.send(sys)
+    await ctx.send("Tous les systèmes disponibles :")
+    sys = None
+    with open(path_sys_txt,'r') as f:
+        sys = [i.strip() for i in f.readlines()]
+    for i in sys:
+        await ctx.send(i)
 
 
-@bot.command(name='roll', aliases=['r'], help='Make a test under a score for a specified character with an optionnal modifier.')
+
+@bot.command(name='roll', aliases=['r'], help='Fait un test sous une compétence données en utilisant un personnage avec un modificateur optionnel.')
 async def lilroll(ctx, character: str, test: str, mod: int=0):
     # bug boucle avec modificateur négatif
     spell = SpellChecker(language=None)
     character = character.lower()
     test = test.lower()
     roll = random.randint(1,100)
-    if roll <= 10: await play(ctx,'./data_sound/victory.mp3')
-    elif roll >= 90 : await play(ctx,'./data_sound/fail.mp3')
-    else : await play(ctx,'./data_sound/dice_sound.mp3')
+    if roll <= 10: await play(ctx,'./data/data_sound/victory.mp3')
+    elif roll >= 90 : await play(ctx,'./data/data_sound/fail.mp3')
+    else : await play(ctx,'./data/data_sound/dice_sound.mp3')
     if len(character) == 1 : character = trans[character]
     path = './data_characters/' + character + '.json'
     result = character + ' rolling for '
@@ -80,7 +168,7 @@ async def lilroll(ctx, character: str, test: str, mod: int=0):
             return
     await ctx.send(result)
 
-@bot.command(name='carac', help='List all caracteristics available to a character.')
+@bot.command(name='carac', help='Liste toutes les caractéristiques disponibles pour le personnage.')
 async def lilroll(ctx, character: str):
     if len(character) == 1 : character = trans[character]
     path = './data_characters/' + character + '.json'
@@ -90,7 +178,7 @@ async def lilroll(ctx, character: str):
         result = "\n".join(data.keys())
     await ctx.send(result)
 
-@bot.command(name='characters', help='List all characters available.')
+@bot.command(name='characters', help='Liste tous les personnages disponibles. (va disparaitre)')
 async def listCharacter(ctx):
     path = os.getcwd() + '/data_characters'
     files = os.listdir(path)
@@ -99,7 +187,7 @@ async def listCharacter(ctx):
     await ctx.send(result)
 
 
-@bot.command(name='shutdown', aliases=['sd','quit'], help='Kill the bot.')
+@bot.command(name='shutdown', aliases=['sd','quit'], help='Termine le bot.')
 async def shutdown(ctx):
     global vc
     try:
@@ -107,11 +195,11 @@ async def shutdown(ctx):
     except:
         pass
     await client.close()
-    ctx.send("Shutdown")
+    await ctx.send("Shutdown")
     print("Bot closed")
     exit(0)
 
-@bot.command(name='dice', aliases=['d'], help='Rolling dices [n°dices]d[n°faces]')
+@bot.command(name='dice', aliases=['d'], help='Lance des dés [n°dés]d[n°faces]')
 async def dice(ctx,dice:str):
     dice = dice.lower()
     dices, faces = map(int,dice.split('d'))
