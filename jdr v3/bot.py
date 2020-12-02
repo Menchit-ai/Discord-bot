537311c3764eea0034b594cd81fa39bf3b2bf55b
menchit <thomas.menchi@edu.esiee.fr>
2020-12-02 15:58:01 +0100

    system fonctionnel avec ajout et suppression des caractéristiques

--- jdr v3/bot.py
+++ jdr v3/bot.py
@@ -12,17 +12,23 @@ from discord.ext import commands
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+PATH = "D:\perso\discordBot\jdr v3\config.json"
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 
-###########  CHEMIN DE BASE  ###########
-path_data     = "./data/"
-path_sound    = "./data/data_sound/"
-path_sys      = "./data/data_sys/"
-path_sys_json = path_sys + "system.json"
-path_sys_txt  = path_sys + "system.txt"
-########################################
+def get_config():
+    data = {}
+    with open(PATH,'r') as json_file:
+        try: data = json.load(json_file)
+        except: return -1
+    return data
+
+def update(data):
+    with open(PATH,'w') as json_file:
+        try: json.dump(data,json_file)
+        except: return -1
+    return 1
 
 async def play(ctx,path):
     try:
@@ -50,46 +56,42 @@ def spellcheck(words):
     return spell
 
 def get_sys(ctx):
-    guild = ctx.guild.name
-    with open(path_sys_json,'r') as json_file:
-        data = json.load(json_file)
-        try : return data[guild]
-        except : return "None"
+    guild = get_guild(ctx)
+    config = get_config()
+    try : return config["curr_sys"][guild]
+    except : return None
+
+def get_user(ctx):
+    return ctx.author.name
+
+def get_guild(ctx):
+    return ctx.guild.name
 
 def get_carac(ctx):
+    config = get_config()
     sys = get_sys(ctx)
-    data = []
-    with open(path_sys + sys + '/' + sys + '.txt', 'r') as _file: data = _file.read().split('|')
-    return data
+    try : return config["sys"][sys]["carac_system"]
+    except : return None
 
-def get_character(ctx):
-    sys = get_sys(ctx)
-    user = ctx.author.name
-    path = path_sys + sys+'/' + 'character.json'
-    data = {}
-    try:
-        with open(path,'r') as json_file : data = json.load(json_file)
-    except : data = None
-    c = "None"
-    try : c = data[user]
-    except : pass
-    return c
-
+# def get_character(ctx):
+#     config = get_config()
+#     sys = get_sys(ctx)
+#     pass
 
 # toutes les commandes disponibles avec le bot
 
 @bot.command(name='createc', aliases=['cc'], help="Commande pour créer son personnage, alias : cc.")
 async def create_character(ctx, name:str):
     sys = get_sys(ctx)
-    user = ctx.author.name
+    user = get_user(ctx)
     if sys is None : await ctx.send("Le serveur n'utilise actuellement aucun système, choisissez-en un !"); return
 
-    path = path_sys + sys + '/' + user + '/'
-    if not os.path.isdir(path) : os.mkdir(path)
+    config = get_config()
     
-    path = path + name + '.json'
+    try : characters = config["system"]["characters"][user][sys].keys()
+    except : characters = []
 
-    if os.path.isfile(path) : await ctx.send("Ce personnage existe déjà."); return
+    if name in characters : await ctx.send("Ce personnage existe déjà."); return
     
     carac = get_carac(ctx)
     if carac[0] == '' : await ctx.send("Le système " + sys + " n'a pas de caractéristique propre."); return
@@ -124,9 +126,9 @@ async def create_character(ctx, name:str):
         except:
             await ctx.send("Il y a eu un problème dans la lecture de votre saisie.")
 
+    config["system"]["characters"][user][sys][name] = stats
 
-    open(path,'w')
-    with open(path, 'w') as json_file : json.dump(stats,json_file)
+    if not update(config) : await ctx.send("Il y a eu un problème dans l'enregistrement des caractéristiques."); return
 
     show_stat = "Le personnage a les caractéristiques suivantes : "
     for key,value in stats.items():
@@ -138,135 +140,106 @@ async def create_character(ctx, name:str):
 @bot.command(name='choose_character', aliases=['cch'], help="Permet à un joueur de choisir le personnage qu'il souhaite utiliser dans le système courant, alis : cch.")
 async def funcname(ctx, character:str):
     sys = get_sys(ctx)
-    path = path_sys + sys+'/' + 'character.json'
-    data = {}
-    with open(path,'r') as json_file : 
-        try : data = json.load(json_file)
-        except:pass
-    user = ctx.author.name
-    _path = path_sys + sys+'/' + user
+    config = get_config()
+    user = get_user(ctx)
+    
+    try: characters = config["system"]["characters"][user][sys].keys()
+    except : characters = []
 
-    if not character in [f.split('.')[0] for f in os.listdir(_path)] : await ctx.send("Le personnage n'est pas disponible."); return
+    if not character in characters : await ctx.send(character + " n'est pas disponible."); return
 
-    data[user] = character
-    with open(path,'w') as json_file : json.dump(data,json_file)
+    config["curr_characters"][user][sys] = character
+
+    if not update(config) : await ctx.send("Il y a eu un problème dans l'enregistrement."); return
     await ctx.send("Le personnage de " + user + " est maintenant " + character + ".")
 
 
 @bot.command(name='add_carac', aliases=['ac'], help='Ajoute des capacités dans la liste des capacités disponibles du système courant, alias : ac.')
 async def add_carac(ctx, *carac:str):
     sys = get_sys(ctx)
-    if sys is "None" : await ctx.send("Choisissez le système courant auquel ajouter les caractéristiques"); return
-    with open(path_sys + sys + '/' + sys + '.txt', 'r') as _file : data = _file.read().split('|')
-    for c in carac: data.append(c)
-    data = [d for d in data if not d==""]
-    with open(path_sys + sys + '/' + sys + '.txt', 'w') as _file : _file.write("|".join(data))
-    await ctx.send("Les caractéristiques suivantes ont été ajoutés au système " + sys + " : " + ", ".join(carac) + '.')
+    config = get_config()
+    if sys is None : await ctx.send("Choisissez le système courant auquel ajouter les caractéristiques"); return
+
+    caracteristics = config["sys"][sys]["carac_system"]
+    for c in carac: caracteristics.append(c)
+    config["sys"][sys]["carac_system"] = caracteristics
+
+    if not update(config) : await ctx.send("Il y a eu un problème dans l'enregistrement."); return
+    await ctx.send("Les caractéristiques suivantes sont présentes dans " + sys + " : \n" + ", ".join(caracteristics) + '.')
 
 @bot.command(name='rm_carac', aliases=['rc'], help='Supprime des capacités dans la liste des capacités disponibles du système courant, alias : rc.')
 async def rm_carac(ctx, *carac:str):
     if not ctx.author.name == "Menchrof" : await ctx.send("Vous n'avez pas la permission de supprimer des systèmes."); return
     sys = get_sys(ctx)
+    config = get_config()
     if sys is None : await ctx.send("Choisissez le système courant duquel supprimer les caractéristiques"); return
-    data = []
-    with open(path_sys + sys + '/' + sys + '.txt', 'r') as _file : data = _file.read().split('|')
-    data = [x for x in data if x not in carac]
-    data = [d for d in data if not d==""]
-    with open(path_sys + sys + '/' + sys + '.txt', 'w') as _file : _file.write('|'.join(data))
-    await ctx.send("Voici les nouvelles caractéristiques du système " + sys + " : " + ", ".join(data) + '.')
+    
+    caracteristics = config["sys"][sys]
+    caracteristics = [x for x in caracteristics if x not in carac]
+    caracteristics = [d for d in caracteristics if not d==""]
+    config["sys"][sys] = caracteristics
+
+    if not update(config) : await ctx.send("Il y a eu un problème dans l'enregistrement."); return
+    await ctx.send("Voici les nouvelles caractéristiques du système " + sys + " : " + ", ".join(caracteristics) + '.')
 
 @bot.command(name='show_carac', aliases=['sc'], help='Montre les capacités disponibles du système courant, alias : sc.')
-async def rm_carac(ctx):
-    sys = get_sys(ctx)
-    if sys is None : await ctx.send("Choisissez le système courant duquel supprimer les caractéristiques"); return
-    data = []
-    with open(path_sys + sys + '/' + sys + '.txt', 'r') as _file : data = _file.read().split('|')
-    if data[0] == '' and len(data) == 1: await ctx.send("Il n'y a pas encore de caractéristiques dans ce système."); return
-    await ctx.send("\n".join(data))
+async def show_carac(ctx):
+    system = get_sys(ctx)
+    config = get_config()
+    if system is None : await ctx.send("Choisissez le système courant duquel supprimer les caractéristiques"); return
+    
+    caracteristics = config["sys"][system]["carac_system"]
+    if len(caracteristics) == 0 : await ctx.send("Il n'y a pas encore de caractéristiques dans ce système."); return
+    await ctx.send("Le système " + system + " possède les caractéristiques suivantes : " + ", ".join(caracteristics))
 
 
 @bot.command(name='system', aliases=['sys'], help='Permet de définir le système de jeu utilisé dans tout le serveur Discord. option : c (nouveau système)')
 async def change_system(ctx, newstate:str, option:str=None):
 
-    with open(path_sys_txt) as f:    sys = [i.strip() for i in f.readlines()]
+    config = get_config()
+    systems = config["sys"].keys()
+    user = get_user(ctx)
+    guild = get_guild(ctx)
     
     if option is "c":
-        if newstate in sys:
-            await ctx.send(newstate + " est déjà présent dans les systèmes utilisables !")
-            return
-        with open(path_sys_txt,'a') as filesys:
-            w = newstate+'\n'
-            filesys.write(w)
-            
-        path = path_sys + newstate
-        try:
-            os.mkdir(path)
-            _path = path + '/' + newstate + '.txt'
-            open(_path,'a').close()
-        except:
-            await ctx.send("Le dossier n'a pas pu être créé.")
+        if newstate in systems: await ctx.send(newstate + " est déjà présent dans les systèmes utilisables !");return
+        
+        config["sys"][newstate] = {"carac_system":[], "characters":{}}
 
-        _path = path + '/' + 'character' + '.json'
-        open(_path,'a').close()
-
+        update(config)
         await ctx.send(newstate + " a bien été ajouté aux systèmes jouables !")
         return
 
-    spell = spellcheck(sys)
+    spell = spellcheck(systems)
     try : newstate = spell.correction(newstate)
     except : pass
 
     if option is "d":
-        with open(path_sys_txt,'w') as filesys:
-            try:
-                sys.remove(newstate)
-            except:
-                await ctx.send(newstate + " ne fait pas partie de la liste.")
-                return
-            for s in sys:
-                w = s+'\n'
-                filesys.write(w)
+        if not ctx.author.name == "Menchrof" : await ctx.send("Vous n'avez pas la permission de supprimer des systèmes."); return
+        if newstate not in systems : await ctx.send(newstate + " ne fait pas partie de la liste."); return
         
-        data = {}
-        with open(path_sys_json,'r') as json_file:
-            try: data = json.load(json_file)
-            except:pass
-        todel = []
-        for key,value in data.items():
-            if value == newstate:
-                todel.append(key)
-        for key in todel:
-            data.pop(key)
-        with open(path_sys_json,'w') as json_file:
-            json.dump(data,json_file)
+        del config["sys"][newstate]
 
-        try:
-            path = path_sys + newstate
-            shutil.rmtree(path)
-        except:
-            await ctx.send("Le dossier n'a pas pu être supprimé.")
+        for key,value in config["curr_sys"].items():
+            if value == newstate : config["curr_sys"][key] = None
+
+        update(config)
 
         await ctx.send(newstate + " a bien été supprimé de la liste.")
         return
 
-    if not (newstate in sys) : await ctx.send("Ce système n'est pas disponible pour le moment."); return
-    data = {}
-    with open(path_sys_json,'r') as json_file:
-        try : data = json.load(json_file)
-        except : data = {}
-    with open(path_sys_json,'w') as json_file:
-        guild = ctx.guild.name
-        data[guild] = newstate
-        json.dump(data,json_file)
-        await ctx.send("le serveur " + guild + " utilise maintenant le système " + newstate + ".")
-        
+    if not (newstate in systems) : await ctx.send("Ce système n'est pas disponible pour le moment."); return
+    config["curr_sys"][guild] = newstate
+    update(config)
+    await ctx.send("le serveur " + guild + " utilise maintenant le système " + newstate + ".")
+    
     return
 
 @bot.command(name='info', aliases=['i'], help='Informations générales.')
 async def show(ctx):
-    user = ctx.author.name
-    guild = ctx.guild.name
-    curr_sys = "None"
+    user = get_user(ctx)
+    guild = get_guild(ctx)
+    curr_sys = get_sys(ctx)
 
     try:
         with open(path_sys_json,'r') as json_file:
@@ -329,9 +302,9 @@ async def lilroll(ctx, test: str, mod: int=0):
     with open(_path_carac,'r') as json_file : data = json.load(json_file)
 
     roll = random.randint(1,100)
-    if roll <= 10: await play(ctx,path_sound+'victory.mp3')
-    elif roll >= 90 : await play(ctx,path_sound+'fail.mp3')
-    else : await play(ctx,path_sound+'dice_sound.mp3')
+    if roll <= 10: await play(ctx,'./data_sound/victory.mp3')
+    elif roll >= 90 : await play(ctx,'./data_sound/fail.mp3')
+    else : await play(ctx,'./data_sound/dice_sound.mp3')
 
     value = data[test]
 
